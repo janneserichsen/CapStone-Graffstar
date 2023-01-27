7b5db371709cf71121b50a319974b521cec9b318
janneserichsen <janneserichsen402@gmail.com>
2023-01-27 11:04:33 +0100

    folder strcuture changed

---
+++ pages/info/index.js
@@ -0,0 +1,91 @@
+import ButtonExample from "../buttonexample";
+import Navbar from "../navbar";
+
+export default function infoPage() {
+  return (
+    <>
+      <h1>Graffstar</h1>
+      <h2>Nice to know</h2>
+      <p>Did you know Rapper Sido has his Name from his Graffiti Name</p>
+      <p>Same for Fler and Ufo 361</p>
+      <p>
+        Cornbread was the first Person to put his Name with Spraypaint on a wall
+        in 1965
+      </p>
+      <p>Graffiti started in NYC</p>
+      <p>
+        Thierry Noir (1958) and Keith Haring (1958-1990)are some of Germanys
+        first Writers
+      </p>
+      <h2>Tipps</h2>
+      <p>Use Fat Caps for faster Work</p>
+      <p>If you want to save Paint use Banana or skinny Caps</p>
+      <p>Use skinny or NYC Caps for Outlines</p>
+      <p>If you have not much Money use Bucket Paint for Filling</p>
+      <p>I think most one know this but make sure to shake your cans well</p>
+      <p>
+        On Halls give you the Time you need if you make small Mistakes fix them
+      </p>
+      <p>Dont Bite other Peoples Stuff or Name !!!!</p>
+      <p>If you Toy please dont start this unnecessary Tagging Beef</p>
+      <h2>Tipps for low Budget and faster Work</h2>
+      <h3>Low Budget</h3>
+      <ul>
+        <li>Use Bucket Paint</li>
+        <li>Cheap Cans</li>
+        <li>Only Good Cans for Outline</li>
+        <li>Seems logic but write small</li>
+        <li>Use skinny or banana Caps</li>
+        <li>Stretch Filling</li>
+        <li>Draw Outliners</li>
+      </ul>
+      <h3>Faster Work</h3>
+      <ul>
+        <li>All kind of Fatcaps</li>
+        <li>Outliners</li>
+        <li>Draw Blockbusters</li>
+        <li>Write together</li>
+      </ul>
+
+      <h2>Writes everybody should know</h2>
+      <h3>Germany</h3>
+      <ul>
+        <li>Emra</li>
+        <li>THC Crew</li>
+        <li>SN</li>
+        <li>Koma</li>
+        <li>SRF Crew / SKE Crew</li>
+        <li>Skore 79</li>
+        <li>Grab</li>
+        <li>MTO</li>
+      </ul>
+      <h3>USA</h3>
+      <ul>
+        <li>Chaka</li>
+        <li>Tracy 168</li>
+        <li>Daze</li>
+        <li>Dondi White</li>
+        <li>Samo</li>
+        <li>Utah</li>
+        <li>Ether</li>
+        <li>Saber</li>
+        <li>Twist</li>
+        <li>Blade</li>
+      </ul>
+      <h3>Russia</h3>
+      <ul>
+        <li>Doke</li>
+        <li>Wekman</li>
+      </ul>
+      <section>
+        <h3>Try out some cool Color Combis</h3>
+        <ButtonExample />
+        <p>Filling</p>
+        <ButtonExample />
+        <p>Outline</p>
+      </section>
+
+      <Navbar />
+    </>
+  );
+}
